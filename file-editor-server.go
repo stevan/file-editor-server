d5085507729d38bff898379c4e595a0f81d5fb96
Stevan Little <stevan.little@iinteractive.com>
2013-10-31 16:09:57 -0400

    move the OAuth config construction to the auth package

--- file-editor-server.go
+++ file-editor-server.go
@@ -5,10 +5,9 @@ import (
     "flag"
     "net/http"
 
-    "code.google.com/p/goauth2/oauth"
-
     "github.com/stevan/httpapp/component"
     "github.com/stevan/httpapp/middleware"
+    "github.com/stevan/httpapp/middleware/auth"
     "github.com/stevan/httpapp/middleware/sessions"
 
     "github.com/stevan/file-editor-server/controller"
@@ -24,16 +23,6 @@ func main() {
     var host = flag.String("host", defaultHost, "the hostname")
     var port = flag.String("port", defaultPort, "the port")
     flag.Parse()
-
-    var oauth_config = &oauth.Config{
-        ClientId:     os.Getenv("DD_OAUTH_CLIENT_ID"),
-        ClientSecret: os.Getenv("DD_OAUTH_CLIENT_SECRET"),
-        AuthURL:      "https://accounts.google.com/o/oauth2/auth",
-        TokenURL:     "https://accounts.google.com/o/oauth2/token",
-        RedirectURL:  "http://" + *host + ":" + *port + "/oauth2callback",
-        Scope:        "https://www.googleapis.com/auth/userinfo.email",
-        TokenCache:   oauth.CacheFile(os.Getenv("DD_OAUTH_CACHE_FILE")),
-    }
 
     session_state := sessions.NewCookieState("session")
     session_state.Path = "/"
@@ -52,7 +41,12 @@ func main() {
                 middleware.HandleSessions(
                     middleware.HandleGoogleOAuthAuthentication(
                         urlmap,
-                        oauth_config,
+                        auth.CreateGoogleOAuthConfig(
+                            os.Getenv("DD_OAUTH_CLIENT_ID"),
+                            os.Getenv("DD_OAUTH_CLIENT_SECRET"),
+                            "http://" + *host + ":" + *port + "/oauth2callback",
+                            os.Getenv("DD_OAUTH_CACHE_FILE"),
+                        ),
                     ),
                     session_state,
                     session_store,
